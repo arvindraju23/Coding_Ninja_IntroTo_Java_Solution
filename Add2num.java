93ab65839dd9e1545fab1474dd3bd9639f2b598b
Arvind Kr Raju <99745659+arvindraju23@users.noreply.github.com>
2022-10-08 14:50:38 +0530

    Add files via upload
    Lecture_1

---
+++ Add2num.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class Add2num {
+	public static void main(String args[]) {
+//		//Data Types are
+//		int i= 10;
+//		int j=20;
+//		System.out.println(i+j);
+		
+//		// data types for Integer value
+//		int k=10;
+//		short s= 132;
+//		long l= 12343;
+//		
+//		// data types for decimal value
+//		double d= 13.35;
+//		float fl=1.3f;
+//		
+//		// data types for character value
+//		char ch= 'a';
+//		
+//		//data types for boolean value
+//		boolean b= true;
+//		boolean otherB= false;
+		
+		
+		// Taking input from user
+		Scanner sc= new Scanner (System.in);
+		int i= sc.nextInt();
+		int j= sc.nextInt();
+		System.out.println(i+j);
+		
+		
+	
+		
+	}
+
+}
---
+++ Relational.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Relational {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan= new Scanner (System.in);
+		int i= scan.nextInt();
+		int j= scan.nextInt();
+		
+		boolean isEq =(i==j);
+		System.out.println(isEq);
+
+	}
+
+}
